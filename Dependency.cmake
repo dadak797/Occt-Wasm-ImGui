cade05d414b7dc8ff6d2a1c9e05317db9073ea90
dadak797 <dadak797@gmail.com>
2022-09-02 22:34:45 +0900

    Modifying Occt Window (in progress)

--- Dependency.cmake
+++ Dependency.cmake
@@ -5,18 +5,18 @@ set(DEP_INCLUDE_DIR ${DEP_INSTALL_DIR}/include)
 set(DEP_LIB_DIR ${DEP_INSTALL_DIR}/lib)
 
 # SPDLOG: Fast Logger Library
-# ExternalProject_Add(
-#     dep_spdlog
-#     GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
-#     GIT_TAG "v1.x"
-#     GIT_SHALLOW 1
-#     UPDATE_COMMAND ""
-#     PATCH_COMMAND ""
-#     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
-#     TEST_COMMAND ""
-# )
-# set(DEP_LIST ${DEP_LIST} dep_spdlog)
-# set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+ExternalProject_Add(
+    dep_spdlog
+    GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
+    GIT_TAG "v1.x"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
+    TEST_COMMAND ""
+)
+set(DEP_LIST ${DEP_LIST} dep_spdlog)
+set(DEP_LIBS ${DEP_LIBS} spdlog$<$<CONFIG:Debug>:d>)
 
 # ImGUI
 set(IMGUI_VERSION "1.88")
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include "WasmOcctView.hpp" 
-#include "AppManager.hpp"
 
 #include <emscripten.h>
 #include <emscripten/html5.h>
 
 
-//! Dummy main loop callback for a single shot.
+// Dummy main loop callback for a single shot.
 extern "C" void onMainLoop()
 {
-  // do nothing here - viewer updates are handled on demand
-  emscripten_cancel_main_loop();
+    // do nothing here - viewer updates are handled on demand
+    emscripten_cancel_main_loop();
 }
 
 EMSCRIPTEN_KEEPALIVE int main()
 {
-    // Initialize Application
-    AppManager& appManager = AppManager::GetInstance();
+    SPDLOG_INFO("Start Application");
 
     // setup a dummy single-shot main loop callback just to shut up a useless Emscripten error message on calling eglSwapInterval()
     emscripten_set_main_loop (onMainLoop, -1, 0);
